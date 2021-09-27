2be9bb52c03ca6d6cd18db79035ae54e42375619
hkpark97 <77339221+hkpark97@users.noreply.github.com>
2021-09-27 16:23:05 -0400

    array/string

---
+++ Array&String/724. Find Pivot Index.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var pivotIndex = function(nums) {
+    let leftSum = 0;
+    let rightSum = 0;
+    
+    for( let i = 1; i < nums.length; i++){
+        rightSum += nums[i];
+    }
+    
+    for( let i = 0; i < nums.length; i++) {
+        let result = nums[i];
+        
+        if(leftSum === rightSum) return i;
+        else{
+            leftSum += result;
+            rightSum -= nums[i+1]
+        }
+    }
+    return -1;
+};
