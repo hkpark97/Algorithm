e55287e7b7ac3938a9caceadf0c999145ff55131
hkpark97 <77339221+hkpark97@users.noreply.github.com>
2021-10-03 09:24:20 -0400

    adjusted codes

--- Array&String/561. Array Partition I.js
+++ Array&String/561. Array Partition I.js
@@ -3,13 +3,12 @@
  * @return {number}
  */
  var arrayPairSum = function(nums) {
-    nums.sort((a, b) => a - b);
-       
-       let length = nums.length;
-       let result = 0;
-       
-       for( let i = 0; i < length; i += 2) {
-           result += nums[i];
-       }
-       return result;
-   };
+    let result = 0;
+      
+   nums.sort((a, b) => a - b);
+      
+      for( let i = 0; i < nums.length; i += 2) {
+          result += nums[i];
+      }
+      return result;
+  };
