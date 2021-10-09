b8dfcd80916529206e6b2c05b479845af704f4be
hkpark97 <77339221+hkpark97@users.noreply.github.com>
2021-10-08 21:32:02 -0400

    added linked list

---
+++ LinkedList/206. Reverse Linked List.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ var reverseList = function(head) {
+    let prevNode = null;
+    
+    while ( head !== null) {
+        let nextNode = head.next;
+        
+        head.next = prevNode;
+        prevNode = head;
+        head = nextNode;               
+    }
+    
+    return prevNode; // prevNode's current value is head
+};
